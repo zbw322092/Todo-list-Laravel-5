6017b92521c86b39511b0569de38936ed1007a36
zbw322092 <bz4e14@soton.ac.uk>
2015-12-08 12:09:27 +0800

    A todo list application in Laravel 5

---
+++ app/Http/routes.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('projects', 'ProjectsController');
+Route::resource('projects.tasks', 'TasksController');
+
+Route::bind('tasks', function($value, $route) {
+	return App\Task::whereSlug($value)->first();
+});
+
+Route::bind('projects', function($value, $route) {
+	return App\Project::whereSlug($value)->first();
+});
